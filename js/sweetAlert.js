00ebe2c9e46fc283fbae739cd809468ead351de6
brayan <brahyantmsanchez@gmail.com>
2021-11-07 22:42:44 -0500

    first commit

---
+++ js/sweetAlert.js
@@ -0,0 +1,23 @@
+Swal.fire({
+	title: 'Bienvenido al Juego'
+});
+	// text:
+	// html:
+	// icon:
+	// confirmButtonText:
+	// footer:
+	// width:
+	// padding:
+	// background:
+	// grow:
+	// backdrop:
+	// timer:
+	// timerProgressBar:
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+		
+	
